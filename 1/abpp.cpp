25955536f306bccc8b0f9acc3b2e7a53f7f7dfea
tonygo5566 <b00902126@ntu.edu.tw>
2014-10-01 11:16:10 +0800

    hw1 ab test

---
+++ 1/abpp.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int a;
+  long long b, c;
+  
+  cin >> a;
+  while( a-- ) {
+    cin >> b >> c;
+    cout << b + c << endl;
+  }
+  return 0;
+}
