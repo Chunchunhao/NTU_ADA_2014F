3f3f3cd3ac2893ca2d61652de24c99219b34bed4
tonygo5566 <b00902126@ntu.edu.tw>
2014-10-11 18:53:14 +0800

    hw1-1 v1

--- hw1/1.cpp
+++ hw1/1.cpp
@@ -1,15 +1,71 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+struct cubic {
+  long long w;
+  long long h;
+};
+
+bool cmp( const cubic a, const cubic b ){
+  if( a.w == b.w)
+    return a.h > b.h;
+  return a.w > b.w;
+}
+
+int DC( vector<cubic>::iterator, vector<cubic>::iterator, int &); 
+
 int main() {
-  int a;
-  long long b, c;
   
-  cin >> a;
-  while( a-- ) {
-    cin >> b >> c;
-    cout << b + c << endl;
+  int tries;
+  cin >> tries;
+  while( tries-- ) {
+    int boxesN;
+
+    // Read boxes to memory 
+    cin >> boxesN;
+    vector<cubic> box; 
+    box.resize(boxesN);
+    for( int i=0; i < boxesN; i++) {
+      cin >> box[i].w >> box[i].h ;
+      if( box[i].w < box[i].h) {
+        int tmp = box[i].h;
+        box[i].h = box[i].w;
+        box[i].w = tmp;
+      }
+    }
+
+    // Sort the Width 
+    sort( box.begin(), box.end(), cmp);
+   
+    int count = 0;
+    DC(box.begin(), box.end()-1, count);
+    // DP and output
+    cout << count << endl;
   }
   return 0;
 }
+
+
+int DC( vector<cubic>::iterator now, vector<cubic>::iterator end, int &acum){
+  if( now == end)
+    return 0;
+  else {
+    // test c++11
+    auto next = now + 1;
+    int inclu;
+    if( (now->w >= next->w) && (now->h >= next->h)){
+      inclu = 1 + DC( next, end, acum);
+      if( now->w == next->w && now->h == next->h )
+        inclu ++;
+      acum += inclu;
+      return inclu;
+    }
+    else
+      inclu = DC( next, end, acum);
+      acum += inclu;
+      return inclu;
+  }
+}
+
--- hw1/2.cpp
+++ hw1/2.cpp
@@ -1,27 +1,77 @@
 #include <iostream>
 #include <vector>
 
-typedef int[2] cubic;
+struct block {
+  int owner;
+  int cash;
+};
+
+struct player {
+  int goal;
+  int cash;
+};
 
 using namespace std;
 
 int main() {
-  int n = 0;
-  cin >> n;
+  int tries = 0;
+  cin >> tries;
   
-  while( n -- ) {
+  while( tries-- ) {
     // Numbet of test Data
-    int box_nu = 0;
-    cin >> box_nu;
-    vector<cubic> list[box_nu];
-
-    // Read Input
-    for(int i=0; i<box_nu; i++) {
-      cin >> list[i][0] >> list[i][1];
+    int plr, blk, evt;
+    cin >> plr >> blk >> evt;
+    vector<block> mine;
+    mine.resize(blk);
+    vector<player> miner;
+    miner.resize(plr);
+  
+    // Goal
+    for( int i=0; i<plr; i++){
+      cin >> miner[i].goal;
+      miner[i].cash = 0;
     }
-    // Process
-    
-    // Write Output
+
+    // Ownership
+    for( int i=0; i<blk; i++){
+      cin >> mine[i].owner;
+      mine[i].cash = 0;
+    }
+
+    // Event
+    while( evt-- ) {
+      cout << "evt: " << evt << endl;
+      int bg, ed, mny;
+      cin >> bg >> ed >> mny;
+      mine[bg-1].cash += mny;
+      if( ed < mine.size() )
+        mine[ed].cash -= mny;
+      cout << "evt: " << evt << " :: " << bg << " , " << ed << " , " << mine.size() << endl;
+    }
+     
+    int mnyItr = 0;
+    for( int i=0; i<blk; i++) {
+      cout << "Mine : ( " << mine[i].owner << ", " << mine[i].cash <<" )" << endl;
+      cout << "ptr : " << mnyItr << endl;
+      cout << "ply : [[ ";
+      for( int j=0; j<plr; j++) {
+        cout << miner[j].cash << " ";
+      }
+      cout << " ]]" << endl;
+      mnyItr += mine[i].cash;
+      miner[mine[i].owner-1].cash += mnyItr;
+    }
+
+    for( int i=0; i<plr; i++) {
+      if( miner[i].cash >= miner[i].goal )
+        cout << miner[i].cash;
+      else
+        cout << "-1";
+      if( i!= plr -1 )
+        cout << " ";
+    }
+    cout << endl;
+
   };
 
   return 0;
